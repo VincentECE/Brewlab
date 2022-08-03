4fcecd399ced28c246dffb503d2197f6a2e72b09
Vince <vincent.tran.ece@gmail.com>
2022-08-03 02:25:43 -0500

    adds Skeleton loader. Changes logo

--- client/src/component/Layout/Layout.jsx
+++ client/src/component/Layout/Layout.jsx
@@ -1,22 +1,22 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../utils/images/logo.svg';
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../utils/images/monopolyman.svg";
 
 export const Layout = (props) => {
   return (
     <>
-    <nav className='navbar layout-padding-1'>
-      <div className="nav-logo-container">
-        <span>
-          <Logo width="50px" height="auto" />
-        </span>
+      <nav className="navbar layout-padding-1">
         <Link to="/" className="link">
-          <h1 className="title-1">Brew Lab</h1>
+          <div className="nav-logo-container">
+            <span>
+              <Logo className="logo" />
+            </span>
+            <h1 className="title-1">Brewopoly</h1>
+          </div>
         </Link>
+      </nav>
+      <div className="layout-padding-1 layout-flex-column-spacing-1">
+        {props.children}
       </div>
-    </nav>
-    <div className="layout-padding-1 layout-flex-column-spacing-1">
-      {props.children}
-    </div>
     </>
-  )
-}
+  );
+};
--- client/src/component/Tile/index.js
+++ client/src/component/Tile/index.js
@@ -1,2 +1,1 @@
 export * from './Tile.jsx';
-export * from './Skeleton.jsx';
